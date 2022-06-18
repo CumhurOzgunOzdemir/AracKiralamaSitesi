7ca603050cd87c5f936144e8b18541a8a43a2e77
Cumhur Özgün Özdemir <86354893+CumhurOzgunOzdemir@users.noreply.github.com>
2022-06-18 12:26:28 +0300

    Add files via upload

---
+++ app/Http/Controllers/Admin/CarController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Car;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class CarController extends Controller
+{
+    public function index()
+    {
+        $datalist=Car::all();
+        return view('admin.cars',['datalist'=>$datalist]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $datalist = Category::with('children')->get();
+        return view('admin.cars_add', ['datalist' => $datalist]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Car;
+
+        $data->title = $request->input('title');
+        $data->keywords = $request->input('keywords');
+        $data->description = $request->input('description');
+        $data->image = $request->input('image');
+        $data->category_id = $request->input('category_id');
+        $data->user_id = Auth::id();
+        $data->detail = $request->input('detail');
+        $data->price = $request->input('price');
+        $data->yakitturu = $request->input('yakitturu');
+        $data->vites = $request->input('vites');
+        $data->capasity = $request->input('capasity');
+        $data->slug = $request->input('slug');
+        $data->status = $request->input('status');
+        $data->image = Storage::putFile('images', $request->file('image'));
+        $data->save();
+
+        return redirect()->route('admin_cars');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Car $car)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $car,$id)
+    {
+        $data=Car::find($id);
+        $datalist = Category::with('children')->get();
+        return view('admin.cars_edit',['data'=>$data,'datalist'=>$datalist]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Car $car,$id)
+    {
+        $data=Car::find($id);
+        $data->title = $request->input('title');
+        $data->keywords = $request->input('keywords');
+        $data->description = $request->input('description');
+        $data->image = $request->input('image');
+        $data->category_id = $request->input('category_id');
+        $data->user_id = Auth::id();
+        $data->detail = $request->input('detail');
+        $data->price = $request->input('price');
+        $data->yakitturu = $request->input('yakitturu');
+        $data->vites = $request->input('vites');
+        $data->capasity = $request->input('capasity');
+        $data->slug = $request->input('slug');
+        $data->status = $request->input('status');
+        if($request->file('image')!=null)
+        {
+            $data->image = Storage::putFile('images',$request->file('image'));
+        }
+
+        $data->save();
+        return redirect()->route('admin_cars');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Car $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $cars,$id)
+    {
+        DB::table('cars')->where('id','=',$id)->delete();
+        return redirect()->route('admin_cars');
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,30 +2,199 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
+use App\Models\Category;
+use App\Models\Faq;
+use App\Models\Image;
+use App\Models\Message;
+
+use App\Models\Reserve;
+use App\Models\Review;
+use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class HomeController extends Controller
 {
-    //
-    public function index()
+
+    public static function getsetting()
     {
-        return view(view: 'home.index');
+        return Setting::first();
+    }
+    public function index(){
+        $setting=Setting::first();
+        $slider=Car::select('id','title','image','slug','price','category_id')->limit(4)->get();
+        $daily=Car::select('id','title','image','slug','price','category_id')->limit(4)->inRandomOrder()->get();
+        $last=Car::select('id','title','image','slug','price','category_id')->orderByDesc('id')->get();
+        $picked=Car::select('id','title','image','slug','price','category_id')->inRandomOrder()->get();
+        $konforlu=Car::select('id','title','image','slug','price','capasity','category_id')->where('category_id','=','2')->inRandomOrder()->get();
+        $data=[
+            'setting'=>$setting,
+            'daily'=>$daily,
+            'last'=>$last,
+            'picked'=>$picked,
+            'slider'=>$slider,
+            'page'=>'home',
+            'konforlu'=>$konforlu
+
+        ];
+        return view('home.index',$data);
+    }
+    public static function categoryList()
+    {
+        return Category::where('parent_id','=',0)->with('children')->get();
     }
 
-    //
-    public function test($id,$name)
+
+    public function car($id,$slug){
+        $setting=Setting::first();
+        $data=Car::find($id);
+        $images=Image::where('car_id',$id)->get();
+        $reviews=Review::where('car_id',$id)->get();
+
+
+        return view('home.car_detail',['setting'=>$setting,'data'=>$data,'images'=>$images,'reviews'=>$reviews]);
+
+    }
+    public function categorycars($id,$slug){
+        $setting=Setting::first();
+        $datalist=Car::where('category_id',$id)->get();
+        $data=Category::find($id);
+
+        return view('home.category_cars',['data'=>$data,'datalist'=>$datalist,'setting'=>$setting]);
+
+    }
+
+    public function aboutus(){
+        return view('home.about');
+    }
+    public function contact(){
+        $setting=Setting::first();
+        return view('home.contact',['setting'=>$setting,'page'=>'home']);
+    }
+
+    public function getcar(Request $request)
     {
-        return view('home.test',['id'=>$id],['name'=>$name]);
-        /*
-        echo "ID NUMBER  :", $id;
-        echo "<br>";
-        echo "NAME  :", $name;
-        for($i = 1;$i<$id;$i++)
+        $search=$request->input('search');
+        $count=Car::where('title','like','%'.$search.'%')->get()->count();
+        if($count==1)
         {
-            echo"<br>$i - $name";
+            $data=Car::where('title','like','%'.$search.'%')->first();
+            return redirect()->route('car',['id'=>$data->id,'slug'=>$data->slug]);
         }
-        */
+        else
+        {
+            return redirect()->route('carlist',['search'=>$search]);
+        }
+
+
+    }
+    public function carlist($search){
+        $datalist=Car::where('title','like','%'.$search.'%')->get();
+
+        return view('home.search_cars',['search'=>$search,'datalist'=>$datalist]);
+
+    }
+    public function references(){
+        $setting=Setting::first();
+        return view('home.references',['setting'=>$setting,'page'=>'home']);
     }
 
 
+    public function sendmessage(Request $request)
+    {
+        $data = new Message();
+
+        $data->name = $request->input('name');
+        $data->email = $request->input('email');
+        $data->phone = $request->input('phone');
+        $data->subject = $request->input('subject');
+        $data->message = $request->input('message');
+
+
+        $data->save();
+
+        return redirect()->route('contact')->with('success','Mesajınız kaydedilmiştir');
+    }
+
+
+    public function sendreview(Request $request,$id)
+    {
+        $data = new Review;
+
+        $data->user_id = Auth::id();
+        $car = Car::find($id);
+        $data->car_id=$id;
+        $data->subject = $request->input('subject');
+        $data->review = $request->input('review');
+        $data->IP = $_SERVER['REMOTE_ADDR'];
+
+        $data->save();
+
+        return redirect()->route('car',['id'=>$car->id,'slug'=>$car->slug])->with('success','Mesajınız kaydedilmiştir');
+    }
+    public function sendreserve(Request $request,$id)
+    {
+        $data = new Reserve;
+        $data->user_id = Auth::id();
+        $car = Car::find($id);
+        $data->returnDate = $request->input('returnDate');
+        $data->rezDate = $request->input('rezDate');
+        $data->rezTime = $request->input('rezTime');
+        $data->returnTime = $request->input('returnTime');
+        $data->car_id = $id;
+        $data->price = $car->price;
+        $datetime1 = strtotime($data->rezDate); // convert to timestamps
+        $datetime2 = strtotime($data->returnDate); // convert to timestamps
+        $data->days = (int)(($datetime2 - $datetime1)/86400);
+
+        $data->amount = $data->price * $data->days;
+
+
+
+        $data->IP = $_SERVER['REMOTE_ADDR'];
+        $data->save();
+
+        return redirect()->route('car',['id'=>$car->id,'slug'=>$car->slug]);
+    }
+
+
+    public function faq(){
+        $setting=Setting::first();
+        $datalist=Faq::all()->sortBy('position');
+        return view('home.faq',['datalist'=>$datalist,'setting'=>$setting]);
+    }
+
+    public function login(){
+        return view('admin.login');
+    }
+    public function logincheck(Request $request)
+    {
+        if($request->isMethod('post'))
+        {
+            $credentials = $request->only('email', 'password');
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+
+                return redirect()->intended('admin');
+            }
+
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+        else
+        {
+            return view('admin.login');
+        }
+    }
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
